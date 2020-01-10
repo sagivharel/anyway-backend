5b45fa834fe3d9ff4d6f32db6d9291c95f798897
arielszabo <arielszabo@gmail.com>
2020-01-10 18:44:24 +0200

    add data_date_string as a default_args

--- processing_pipeline/dags/anyway_processing.py
+++ processing_pipeline/dags/anyway_processing.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+import os
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
@@ -10,24 +12,22 @@ from utils import concat_processed_data
 
 DATA_DATE_STRING = "20191161"
 
-dag = DAG('anyway_processing_pipeline', start_date=datetime(2020, 1, 1))
+dag = DAG('anyway_processing_pipeline', start_date=datetime(2020, 1, 1), concurrency=os.cpu_count(),
+          default_args={"op_kwargs": {"data_date_string": DATA_DATE_STRING}})
 
 transformation__create_xy_column = PythonOperator(task_id='transformation__create_xy_column',
                                                   python_callable=create_xy_column,
-                                                  op_kwargs={"data_date_string": DATA_DATE_STRING},
                                                   dag=dag)
 
 
 cleaning__replace_minus_one_with_null__mahoz = PythonOperator(task_id='cleaning__replace_minus_one_with_null__mahoz',
                                                               python_callable=replace_minus_one_with_null,
-                                                              op_kwargs={"data_date_string": "20191161",
-                                                                         "column_name": MAHOZ},
+                                                              op_kwargs={"column_name": MAHOZ},
                                                               dag=dag)
 
 
 utils__concat_processed_data = PythonOperator(task_id='utils__concat_processed_data',
                                               python_callable=concat_processed_data,
-                                              op_kwargs={"data_date_string": "20191161"},
                                               dag=dag)
 
 transformation__create_xy_column.set_downstream(utils__concat_processed_data)
