7a67c24978df387615172869c25dbfcdea8f6828
arielszabo <arielszabo@gmail.com>
2020-01-19 18:51:36 +0200

    wip

--- processing_pipeline/cleaning/missing_and_unknown_values.py
+++ processing_pipeline/cleaning/missing_and_unknown_values.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from utils import load_data, save_processed_data, get_saved_file_path
 from constants import TASK
-"""
-This is just an example code for a simple cleaning task
-"""
+
+
+# TODO This is just an example code for a simple cleaning task
 def replace_minus_one_with_null(file_name, column_name, **context):
+
     df = load_data(file_name, raw_data=True)
     clean_series = df[column_name].replace(-1, None)
     clean_df = pd.DataFrame(clean_series, columns=[column_name])
--- processing_pipeline/transformation/new_columns_creation.py
+++ processing_pipeline/transformation/new_columns_creation.py
@@ -2,9 +2,8 @@ import pandas as pd
 from utils import save_processed_data, get_saved_file_path, get_upstream_tasks_output_concat_df
 from constants import X, Y, XY, TASK
 
-"""
-This is just an example code for a simple transformation task
-"""
+
+# TODO This is just an example code for a simple transformation task
 def create_xy_column(**context):
     df = get_upstream_tasks_output_concat_df(context, remove_none_values=True)
     xy_series = df[X].astype(str) + "/" + df[Y].astype(str)
