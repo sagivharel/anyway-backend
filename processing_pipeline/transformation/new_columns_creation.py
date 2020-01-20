c72efd269d5be6ff22ea058f5b5276dc15656b0f
arielszabo <arielszabo@gmail.com>
2020-01-20 08:59:13 +0200

    ass a header

--- processing_pipeline/transformation/new_columns_creation.py
+++ processing_pipeline/transformation/new_columns_creation.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from utils import save_processed_data, get_saved_file_path, get_upstream_tasks_output_concat_df
 from constants import X, Y, XY, TASK
 
@@ -7,6 +6,7 @@ from constants import X, Y, XY, TASK
 def create_xy_column(**context):
     df = get_upstream_tasks_output_concat_df(context, remove_none_values=True)
     xy_series = df[X].astype(str) + "/" + df[Y].astype(str)
+    xy_series.name = XY
 
     saved_file_path = get_saved_file_path(context[TASK].task_id)
     save_processed_data(xy_series, saved_file_path, use_index=False)
